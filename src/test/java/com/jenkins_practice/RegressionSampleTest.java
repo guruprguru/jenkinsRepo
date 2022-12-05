195cfcb406b112b37346483af0639c7f0ec94464
gurup <gurup@LAPTOP-MHBSPLVF>
2022-12-05 17:06:20 +0530

    jenkins

---
+++ src/test/java/com/jenkins_practice/RegressionSampleTest.java
@@ -0,0 +1,13 @@
+package com.jenkins_practice;
+
+import org.testng.annotations.Test;
+
+public class RegressionSampleTest
+{
+   @Test
+   public void RegressTest()
+   {
+	 System.out.println("RegressionSuite");  
+   }
+   
+}
---
+++ src/test/java/com/jenkins_practice/SmokeSampleTest.java
@@ -0,0 +1,12 @@
+package com.jenkins_practice;
+
+import org.testng.annotations.Test;
+
+public class SmokeSampleTest 
+{
+@Test
+public void jenkinSmokeTest()
+{
+	System.out.println("Execute smokesuite");
+}
+}
